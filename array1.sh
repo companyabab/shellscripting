e42926710c3882b8416453f3ad43440639e0e662
root <root@instance-1.c.macro-centaur-131400.internal>
2017-08-28 17:09:38 +0000

    array file

---
+++ array1.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+echo 
+ARRAY=(one two three)
+echo ${ARRAY[*]}
+echo ${#ARRAY}
+unset ARRAY[1]
+echo ${ARRAY[0]}
+echo ${ARRAY[1]}
+echo ${ARRAY[2]}
+echo ${#ARRAY}
+echo ${ARRAY[2]}
+ ARRAY[3]=four
+echo ${ARRAY[0]}
+echo ${ARRAY[1]}
+echo ${ARRAY[2]}
+echo ${ARRAY[3]}
+echo ${#ARRAY}
+echo ${ARRAY[*]}
+ARRAY=five
+echo ${ARRAY[*]}
+unset ARRAY
+echo "balance:" ${ARRAY[*]}
+echo ${#ARRAY}
